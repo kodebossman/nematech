9a95bd701bd29b50602cb0c6294e51af66eb3df2
kudakwashe <kuda@nightsbridge.co.za>
2021-07-11 20:08:38 +0200

    The initial application and the model developed

---
+++ src/main/java/com/nematech/umsebenzi/UmsebenziApplication.java
@@ -0,0 +1,13 @@
+package com.nematech.umsebenzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmsebenziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmsebenziApplication.class, args);
+	}
+
+}
